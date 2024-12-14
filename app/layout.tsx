f50d5992c105edec58125a22d89eb0258c1e3a1c
Taewoo <confort28@gmail.com>
2024-12-14 23:12:16 +0900

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import { Metadata } from "next";
 import Navigation from "../components/navigation";
 import "../styles/global.css";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: {
     // 객체일 수 있음
     template: "%s | Next Movies",
     default: "Loading...",
   },
   description: "The best moives on the best framework",
+  favicon: "/favicon.ico",
 };
 
 export default function RootLayout({
--- components/movie.tsx
+++ components/movie.tsx
@@ -12,7 +12,7 @@ interface IMovieProps {
 export default function Movie({ title, id, poster_path }: IMovieProps) {
   const router = useRouter();
   const onClick = () => {
-    router.push(`/moives/${id}`);
+    router.push(`/movies/${id}`);
   };
   return (
     <div className={styles.movie}>
